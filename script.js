d7423b7793fa68d3c0f3866da030710890418260
Josh Broughton <josh.broughton47@gmail.com>
2021-05-29 12:55:58 -0600

    started working on DOM method to display book list as a table

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ function addBookToLibrary(newBook) {
 }
 
 let theHobbit = new Book("The Hobbit", "JRR Tolkien", "295", "read")
+let endersGame = new Book("Ender's Game", "Orson Scott Card", "324", "read")
+let bloodMeridian = new Book("Blood Meridian", "Cormac McCarthy", "337", "not read")
+
+addBookToLibrary(theHobbit)
+addBookToLibrary(endersGame)
+addBookToLibrary(bloodMeridian)
+
+let table = document.getElementById("bookTable");
+
+for (let i=0;i++;i<myLibrary.length) {
+    let row = table.insertRow(-1);
+    let cell = row.insertCell(0);
+    cell.innerHTML = myLibrary[i].info;
+}
