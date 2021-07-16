37cc5b9928ff1a6ec18284f81fbfc815fb733175
Josh Broughton <josh.broughton47@gmail.com>
2021-07-15 18:46:22 -0600

    added onclick for button to add new book to table

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ addBookToLibrary(theHobbit)
 addBookToLibrary(endersGame)
 addBookToLibrary(bloodMeridian)
 
+//function to create html table of book library. iterates
+//over library and inserts rows and cells with the info
 function tableMaker() {
     for (let i=0;i<myLibrary.length;i++) {
         let table = document.getElementById("bookTable");
@@ -38,3 +40,26 @@ function tableMaker() {
     }    
 }
 tableMaker();
+//TODO: add onclick function to add new books to library
+function buttonClick() {
+    let button = document.getElementById("newBook")
+    button.onclick = function() {
+        let title = prompt("Enter the book title", "");
+        let author = prompt("Enter the author's name", "");
+        let pages = prompt("Enter the number of pages", "");
+        let read = prompt('Enter "Read" if you have read the book, or "Not Read" otherwise', "");
+        let aNewBook = new Book(title, author, pages, read);
+        addBookToLibrary(aNewBook);
+        let table = document.getElementById("bookTable");
+        let row = table.insertRow();
+        let cell1 = row.insertCell();
+        let cell2 = row.insertCell();
+        let cell3 = row.insertCell();
+        let cell4 = row.insertCell();
+        cell1.innerHTML = aNewBook.title;
+        cell2.innerHTML = aNewBook.author;
+        cell3.innerHTML = aNewBook.pages;
+        cell4.innerHTML = aNewBook.read;
+    }
+}
+buttonClick()
